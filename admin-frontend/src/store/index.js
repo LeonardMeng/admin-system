b5898d343d4cda90fa58b008c964e46dd19a6455
KanadeM <575628304@qq.com>
2023-06-12 01:06:49 +1000

    update

--- admin-frontend/src/store/index.js
+++ admin-frontend/src/store/index.js
@@ -1,27 +1,8 @@
 import { createStore, applyMiddleware } from 'redux'
 import reduxThunk from 'redux-thunk'
-// import reducer from './reducers'
+import reducer from './reducers'
 
-const store = createStore({
-    state: {
+const store = createStore(reducer, applyMiddleware(reduxThunk));
 
-    },
-    getters: {
-        GET_TOKEN: state => {
-            return sessionStorage.getItem('token')
-        }
-    },
-    mutations: {
-        SET_TOKEN: (state, token) => {
-            sessionStorage.setItem('token', token)
-        }
-    },
-    actions: {
-
-    },
-    modules: {
-
-    }
-});
 
 export default store
